5d96e934c0f226d68fd2f5ac8a53554faf379045
alex <skachkov.ap@phystech.edu>
2020-08-17 23:17:52 +0300

    working version

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -3,13 +3,9 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@EnableWebSecurity
-@ComponentScan("classpath:")
 @EnableJpaRepositories
 public class Application {
     public static void main(String[] args) {
